e51740fbf3813db3ea3d8124ab3f2eeb7ae227ea
Carrie <carrieyu@DESKTOP-N1A0LCS.localdomain>
2023-04-17 11:03:27 -0400

    added files

---
+++ funcs.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "funcs.h"
+
+/*Tasks A and B: Zamansky Instructions
+1. Write these functions in a .cpp file, run from main and test through 2. tests via doctests.
+There should be no user input.
+*/
+
+/*Task A
+1. int minutesSinceMidnight(Time time); return the number of minutes from 0:00AM until time
+2. int minutesUntil(Time earlier, Time later); {10,30}, {13, 40}-> {10:30AM}, (13:40PM)-> 190 minutes in between the two times
+*/
+
+/*Task B
+- 
+*/
+
+
+//--------------------------------------------------------------------
+/*Task C: Zamansky Instructions
+1. The printTimeSlot function should be renamed getTimeSlot and it should return a std::string. Run from main and test via doctests.
+*/
+
+/*Task C
+- 
+*/
+//--------------------------------------------------------------------
+/*Task D: Zamansky Instructions
+1. Write and test these as per usual - that is, no user input, run from main, test via doctest.
+*/
+
+
+
---
+++ funcs.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#pragma once
+
+//datatype (class) for storing time HH:MM in the 24-hour format
+class Time { 
+public:
+    int h; //h for the number of hours
+    int m; //m for the number of minutes
+};
+
